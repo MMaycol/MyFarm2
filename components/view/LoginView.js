a4f7e42586866cb113ccba56f7a4ad74d420b093
MMaycol <103394658+MMaycol@users.noreply.github.com>
2022-06-18 10:57:46 -0600

    Diseño de la vista login

--- components/view/LoginView.js
+++ components/view/LoginView.js
@@ -1,13 +1,23 @@
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity , TextInput, Image} from 'react-native';
 
 const LoginView = (props) => {
 
     return (<View style={styles.CardStyle}>
-        <Text style={styles.Title}>Acceso</Text>
-        <View style={styles.OptionContainer}>
-            <Button title = "Iniciar sesion" onPress={() => {
+        <Text style = {styles.Title}>Acceso</Text>
+        <Text style = {styles.Banner}>MI GRANJA #2</Text>
+
+        <View style = {styles.body_login}>
+
+            <TextInput style = {styles.input} placeholder = "Usuario" />
+            <TextInput style = {styles.input} placeholder = "Contraseña" />
+
+        <TouchableOpacity onPress={() => {
                 props.navigation.navigate('MainView');
-            }}></Button>
+            }}
+            style = {styles.Button} >
+            <Text style = {styles.ButtonText}>Iniciar sesion</Text>
+       </TouchableOpacity>
+
         </View>
         
     </View>);
@@ -18,14 +28,48 @@ export { LoginView }
 const styles = StyleSheet.create({
     CardStyle: {
         flex: 1,
-        backgroundColor: '#999',
+        backgroundColor: 'white',
         padding: 20
-    }, Title: {
+    },
+    Title: {
         flex: 1,
-        color: "white",
-        fontSize: 26,
+        color: "black",
+        fontSize: 30,
         justifyContent: 'center'
-    }, OptionContainer: {
-        flex: 3
+    },
+    Banner: {
+        flex: 1,
+        color: "black",
+        fontSize: 26,
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+    input: {
+        height: 42,
+        margin: 12,
+        fontSize: 20,
+        padding: 6,
+        backgroundColor: '#e0e0e0',
+        borderRadius: 10
+      },
+    Button: {
+        marginRight: 14,
+        marginLeft: 14,
+        marginTop: 20,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: 'black',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'white'
+    },
+    ButtonText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 19
+      },
+    body_login: {
+        marginTop: 10,
+       flex: 4
     }
 });
