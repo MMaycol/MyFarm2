934b5e6c3e9ae2ad07f48b974ccb9221d22b4790
MMaycol <103394658+MMaycol@users.noreply.github.com>
2022-06-24 13:39:46 -0600

    Update modulo compra

--- components/utility/CardDetalleCompraView.js
+++ components/utility/CardDetalleCompraView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, ActivityIndicator, ScrollView, TextInput } from 'react-native';
 import { TblDetalleCompra } from '../../model/TblDetalleCompra.js';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 //Componentes
 
@@ -11,8 +12,17 @@ class CardDetalleCompraView extends React.Component {
         
         this.state = {
             isLoading: true,
-            Dataset: []
+            Dataset: [],
+            HeadTable: ['Nombre de producto', 'Cantida', 'Subtotal'],
+            DataTable: [
+              ['1', '2', '3', '4', '5'],
+              ['a', 'b', 'c', 'd', 'e'],
+              ['1', '2', '3', '4', '5'],
+              ['a', 'b', 'c', 'd', 'e'],
+              ['1', '2', '3', '4', '5']
+            ]
         }
+
      this.CargarProducto(this.props.data)
     }
 
@@ -27,11 +37,15 @@ class CardDetalleCompraView extends React.Component {
     render() {
 
         return (<View>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#ffa1d2'}}>
+           <Row data = {this.state.HeadTable} style = {styles_table.HeadStyle} textStyle = {styles_table.TableText}/>
+           <Rows data = {this.state.DataTable} textStyle = {styles_table.TableText}/>
+           </Table>
              {
             this.state.Dataset.map(m => 
                 <Text key = {m.PKProducto} style = {styles.Atribute}>Nombre de producto: {m.NombreProducto}</Text> )
              }
-            <Text>1</Text>
+            
         </View>)
     }
 }
@@ -58,4 +72,21 @@ const styles = StyleSheet.create({
         borderRadius: 10
       },
     
+  });
+
+  const styles_table = StyleSheet.create({
+    container: { 
+      flex: 1,
+      padding: 18,
+      paddingTop: 35,
+      backgroundColor: '#ffffff' 
+    },
+    HeadStyle: { 
+      height: 50,
+      alignContent: "center",
+      backgroundColor: '#ffe0f0'
+    },
+    TableText: { 
+      margin: 10
+    }
   });
